971ca14a9d00a2dbdd7c95aa0a6f779cd50fcb30
Maxime <mcluzel@edenschool.fr>
2022-06-21 17:06:14 +0200

    Create objects array with data + DOM: Done ToDo: Fix erros in JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,66 @@
 import '../css/style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const section = document.getElementById('catalog');
+
+
+const data = [
+
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  },
+  {
+    banner: "img/film1.jpg",
+    title: "Film1",
+    date: "02 Jan 2022",
+    note: 70%
+  }
+]
+
+for (let i = 0; i < data.length; i++) {
+  section.innerHTML += ` <article class="movie">
+  <a href="#"><img src="${data[i].img}" alt="film"></a>
+  <p class="note">${data[i].note}</p>
+  <h2>${data[i].title}</h2>
+  <p>${data[i].date}</p>
+</article>` 
+  
+}
