1b2341089e576078e59e32974e662a427d122f98
Max-691 <mcluzel@edenschool.fr>
2022-06-21 20:54:13 +0200

    Fix errors: Done, Doing: Adding color in terms of note

--- js/main.js
+++ js/main.js
@@ -4,63 +4,66 @@ const section = document.getElementById('catalog');
 
 
 const data = [
-
   {
-    banner: "img/film1.jpg",
+    banner: "../img/film1.jpg",
     title: "Film1",
     date: "02 Jan 2022",
-    note: 70%
+    note: 70
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film2.jpg",
+    title: "Film2",
     date: "02 Jan 2022",
-    note: 70%
+    note: 10
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film3.jpg",
+    title: "Film3",
     date: "02 Jan 2022",
-    note: 70%
+    note: 45
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film4.jpg",
+    title: "Film4",
     date: "02 Jan 2022",
-    note: 70%
+    note: 30
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film5.jpg",
+    title: "Film5",
     date: "02 Jan 2022",
-    note: 70%
+    note: 85
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film6.jpg",
+    title: "Film6",
     date: "02 Jan 2022",
-    note: 70%
+    note: 90
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film7.jpg",
+    title: "Film7",
     date: "02 Jan 2022",
-    note: 70%
+    note: 55
   },
   {
-    banner: "img/film1.jpg",
-    title: "Film1",
+    banner: "img/film8.jpg",
+    title: "Film8",
     date: "02 Jan 2022",
-    note: 70%
+    note: 60
   }
 ]
 
 for (let i = 0; i < data.length; i++) {
   section.innerHTML += ` <article class="movie">
-  <a href="#"><img src="${data[i].img}" alt="film"></a>
-  <p class="note">${data[i].note}</p>
+  <a href="#"><img src="${data[i].banner}" alt="film"></a>
+  <p class="note">${data[i].note} %</p>
   <h2>${data[i].title}</h2>
   <p>${data[i].date}</p>
 </article>` 
   
+}
+
+if (data[i].note >= 25) {
+  data[i].note.classList.add('note')
 }
